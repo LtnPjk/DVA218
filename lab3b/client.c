01e53e04fccd9196ff7668ece07a4a45c1eb5537
felixsjo <felix.sjqv@gmail.com>
2019-05-14 09:23:28 +0200

    qowidj

--- lab3b/client.c
+++ lab3b/client.c
@@ -1,3 +1,11 @@
+/* File: client.c
+ * Trying out socket communication between processes using the Internet protocol family.
+ * Usage: client [host name], that is, if a server is running on 'lab1-6.idt.mdh.se'
+ * then type 'client lab1-6.idt.mdh.se' and follow the on-screen instructions.
+ * Authors: Felix Sjöqvist and Olle Olofsson
+ * 2019-05-02
+ */
+
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -9,6 +17,6 @@
 #include <netdb.h>
 #include <pthread.h>
 
-int main(int argc, char *argv[]){
-printf("hej");
+int main(int argc, char *argv[]) {
 }
+
---
+++ lab3b/server.c
@@ -0,0 +1,20 @@
+/* File: server.c
+ * Trying out socket communication between processes using the Internet protocol family.
+ * Authors: Felix Sjöqivst and Olle Olofsson
+ * 2019-05-02
+ */
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/times.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main(int argc, char *argv[]) {
+}
+
