1ad8869db25b75b822b38535217cac214b111173
felixsjo <felix.sjqv@gmail.com>
2019-05-15 14:03:55 +0200

    client.c

--- lab3b/client.c
+++ lab3b/client.c
@@ -21,48 +21,115 @@
 #define message 1024
 #define MAXLINE 1024
 
+#define START 0
+#define R_WAIT 1
+#define W_WAIT 2
+#define DONE 3
+
 typedef struct hd_struct {
     int flags;
     int ACK;
     int seq;
     int windowsize;
-    int crc;
+    uint16_t crc;
     char *data;
+    int length;
+    int id;
 } hd;
 
+hd ok;
+int sockfd;
+
+int TWH_loop(){
+    int state = START;
+    while(1){
+        switch(state){
+            case START:
+                /* send SYN=x to server */
+                break;
+            case R_WAIT:
+                /* if we recieve SYN=y and ACK=x+1, send ACK=y+1 and WIN_SIZE to server */
+                /* if timeout -> resend the SYN=x */
+                break;
+            case W_WAIT:
+                /* if recieve WIN_SIZE and id -> send ACK=x+2 */
+                /* if timeout -> resend ACK=y+1 and WIN_SIZE */
+                break;
+            case DONE:
+                /* if we entered the DONE state we want to go to the SW state */
+                return 1;
+
+            default:
+                break;
+
+        }
+    }
+}
+
+int SW_loop(){
+
+}
+
+int TD_loop(){
+
+}
+
 int main(int argc, char *argv[]) {
-    int sockfd; 
+#define TWH 0
+#define SW 1
+#define TD 2
+
+    ok.flags = 0;
+    ok.ACK = 0;
+    ok.seq = 0;
+    ok.windowsize = 4;
+    ok.crc = 0;
+    ok.data = "";
     char buffer[MAXLINE]; 
     char *hello = "Hello from client"; 
     struct sockaddr_in     servaddr; 
-  
+    int state = 0;
+
     // Creating socket file descriptor 
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
     } 
-  
+
     memset(&servaddr, 0, sizeof(servaddr)); 
-      
+
     // Filling server information 
     servaddr.sin_family = AF_INET; 
     servaddr.sin_port = htons(PORT); 
     servaddr.sin_addr.s_addr = INADDR_ANY; 
-      
-    int n, len; 
-      
-    sendto(sockfd, (const char *)hello, strlen(hello), 
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
-            sizeof(servaddr)); 
-    printf("Hello message sent.\n"); 
-          
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
-                MSG_WAITALL, (struct sockaddr *) &servaddr, 
-                &len); 
-    buffer[n] = '\0'; 
-    printf("Server : %s\n", buffer); 
-  
-    close(sockfd); 
-    return 0; 
- }
+
+    while(1){
+        switch(state){
+            case TWH:
+                state = TWH_loop();
+                break;
+            case SW:
+                state = SW_loop();
+                break;
+            case TD:
+                state = TD_loop();
+                break;
+        }
+    }
+    /* int n, len; */ 
+
+    /* sendto(sockfd, &ok, sizeof(hd), */ 
+    /*         MSG_CONFIRM, (const struct sockaddr *) &servaddr, */  
+    /*         sizeof(servaddr)); */ 
+    /* printf("Hello message sent.\n"); */ 
+
+    /* n = recvfrom(sockfd, (char *)buffer, MAXLINE, */  
+    /*         MSG_WAITALL, (struct sockaddr *) &servaddr, */ 
+    /*         &len); */ 
+    /* buffer[n] = '\0'; */ 
+    /* printf("Server : %s\n", buffer); */ 
+
+    /* close(sockfd); */ 
+    /* return 0; */ 
+}
 
