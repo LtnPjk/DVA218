1eeab24a17e7a1b4cdbaa38c05143ef18dc712e6
felixsjo <felix.sjqv@gmail.com>
2019-05-15 11:37:59 +0200

    qowidj

--- lab3b/client.c
+++ lab3b/client.c
@@ -17,6 +17,52 @@
 #include <netdb.h>
 #include <pthread.h>
 
+#define PORT 5555
+#define message 1024
+#define MAXLINE 1024
+
+typedef struct hd_struct {
+    int flags;
+    int ACK;
+    int seq;
+    int windowsize;
+    int crc;
+    char *data;
+} hd;
+
 int main(int argc, char *argv[]) {
-}
+    int sockfd; 
+    char buffer[MAXLINE]; 
+    char *hello = "Hello from client"; 
+    struct sockaddr_in     servaddr; 
+  
+    // Creating socket file descriptor 
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+  
+    memset(&servaddr, 0, sizeof(servaddr)); 
+      
+    // Filling server information 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_port = htons(PORT); 
+    servaddr.sin_addr.s_addr = INADDR_ANY; 
+      
+    int n, len; 
+      
+    sendto(sockfd, (const char *)hello, strlen(hello), 
+        MSG_CONFIRM, (const struct sockaddr *) &servaddr,  
+            sizeof(servaddr)); 
+    printf("Hello message sent.\n"); 
+          
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE,  
+                MSG_WAITALL, (struct sockaddr *) &servaddr, 
+                &len); 
+    buffer[n] = '\0'; 
+    printf("Server : %s\n", buffer); 
+  
+    close(sockfd); 
+    return 0; 
+ }
 
